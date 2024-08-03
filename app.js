df49f6fcefeef04d372f7f4d7f27813f32d3a804
plant0322 <159232279+plant0322@users.noreply.github.com>
2024-08-03 03:26:05 +0000

    [Update]クイズ追加

--- app.js
+++ app.js
@@ -1,29 +1,64 @@
-const question = '目を覚ますとそこは無人島でした';
-const answers = [
-  '近くにあった小屋を探索する',
-  '森の中を探索する',
-  'その場で助けを待つ',
-  'いったん寝る',
+const quiz = [
+  {
+    question: '目を覚ますとそこは無人島でした',
+    answers: [
+      '近くにあった小屋を探索する',
+      '森の中を探索する',
+      'その場で助けを待つ',
+      'いったん寝る'
+    ],
+    correct: '森の中を探索する'
+  }, {
+    question: '森の中で何を見つけましたか？',
+    answers: [
+      '食べれそうな果物',
+      '火を起こせそうな木の枝',
+      'けもの道',
+      '水飲み場'
+    ],
+    correct: 'けもの道'
+  }, {
+    question: '道の先に巨大なビーバーがいました。あなたはどうしますか？',
+    answers: [
+      '逃げる',
+      '攻撃する',
+      '捕まえる',
+      '助けを求める'
+    ],
+    correct: '助けを求める'
+  },
 ];
-const correct = '近くにあった小屋を探索する';
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
 
 const $button = document.getElementsByTagName('button');
+const btnLength = $button.length;
 
 // 問題をHTMLに反映
 const setupQuiz = () => {
-  document.getElementById('js-question').textContent = question;
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
   let btnIndex = 0;
-  let btnLength = $button.length;
   while (btnIndex < btnLength){
-    $button[btnIndex].textContent = answers[btnIndex];
+    $button[btnIndex].textContent = quiz[quizIndex].answers[btnIndex];
     btnIndex++;
   }
 }
 setupQuiz();
 
 const clickHandler = (e) => {
-  if(correct === e.target.textContent){
-    window.alert('次の選択肢へ');
+  if(quiz[quizIndex].correct === e.target.textContent){
+    score++;
+    quizIndex++;
+
+    window.alert('ステージ' + score + 'クリア。次の選択肢へ');
+
+    if(quizIndex < quizLength){
+      setupQuiz();
+    } else {
+      window.alert('season1クリア。次回へ続く');
+    }
   } else {
     window.alert('脱出失敗');
   }
@@ -31,7 +66,6 @@ const clickHandler = (e) => {
 
 // 判定
 let handlerIndex = 0;
-const btnLength = $button.length;
 while (handlerIndex < btnLength){
   $button[handlerIndex].addEventListener('click', (e) => {
     clickHandler(e);
